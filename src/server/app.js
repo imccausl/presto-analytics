84e8e570a78450d74b0380e077355827de40093d
Ian McCausland <imccausl@gmail.com>
2019-01-01 22:46:18 -0500

    Set up body-parser

--- src/server/app.js
+++ src/server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const { Server } = require('http');
+const bodyParser = require('body-parser');
 
 const router = express.Router();
 const app = express();
@@ -10,9 +11,11 @@ const http = Server(app);
 const PORT = process.env.SERVER_PORT || 3000;
 
 router.post('/presto/login', (req, res) => {
-  console.log(req.params);
+  console.log(req.body);
+  res.sendStatus(200);
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1', router);
 
 app.get('/', (req, res) => {
