41543ada6d6cb8100ea288fba7452ecbe4ea33df
Ian McCausland <imccausl@gmail.com>
2019-01-02 21:42:30 -0500

    Add basic routes for grabbing required presto data

--- src/server/app.js
+++ src/server/app.js
@@ -4,15 +4,42 @@ const express = require('express');
 const { Server } = require('http');
 const bodyParser = require('body-parser');
 
+const { login, isLoggedIn, getBasicAccountInfo, getUsageReport } = require('../presto');
+
 const router = express.Router();
 const app = express();
 const http = Server(app);
 
 const PORT = process.env.SERVER_PORT || 3000;
 
-router.post('/presto/login', (req, res) => {
-  console.log(req.body);
-  res.sendStatus(200);
+router.post('/presto/login', async (req, res) => {
+  const prestoCredentials = req.body;
+
+  if (!prestoCredentials.username && !prestoCredentials.password) {
+    res.sendStatus(500);
+    console.log('Invalid request body.');
+    return;
+  }
+
+  try {
+    const prestoLoginResp = await login(prestoCredentials.username, prestoCredentials.password);
+    const accountInfo = await getBasicAccountInfo();
+    console.log(prestoLoginResp);
+    res.send(accountInfo);
+  } catch (error) {
+    res.send({ error });
+  }
+});
+
+router.post('/presto/usage/:year', async (req, res) => {
+  try {
+    const usage = await getUsageReport(req.params.year);
+
+    console.log(`Getting usage report for ${req.params.year}...`);
+    res.send(usage);
+  } catch (error) {
+    res.send({ error });
+  }
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
