fb674d651770dcb96effd8e3411ab74c83c8628e
Ian McCausland <imccausl@gmail.com>
2019-01-05 19:24:48 -0500

    Remove proxy requirement for dev environment

--- src/presto/index.js
+++ src/presto/index.js
@@ -6,12 +6,8 @@ const apiRequestWrapper = require('./api_wrapper');
 const API = require('./api_endpoints');
 
 const jar = req.jar();
-let proxy = false;
 
-if (process.env.NODE_ENV === 'Dev') {
-  proxy = 'http://127.0.0.1:8080';
-}
-const options = { jar, baseUrl: API.baseUrl, proxy };
+const options = { jar, baseUrl: API.baseUrl };
 const request = promisify(req.defaults(options));
 
 module.exports = apiRequestWrapper(request);
