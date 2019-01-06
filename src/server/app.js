caa09cb6fe012695ea7a7653c4ce1f543f879511
Ian McCausland <imccausl@gmail.com>
2019-01-06 12:21:49 -0500

    Experimenting with db/save transactions to db transactions table

--- src/server/app.js
+++ src/server/app.js
@@ -3,15 +3,40 @@ require('dotenv').config();
 const express = require('express');
 const { Server } = require('http');
 const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 
 const { login, isLoggedIn, getBasicAccountInfo, getUsageReport } = require('../presto');
+const dbConfig = require('./db/config');
 
 const router = express.Router();
 const app = express();
 const http = Server(app);
+const sequelize = new Sequelize('analytics', 'postgres', 'postgres', dbConfig);
 
-const PORT = process.env.SERVER_PORT || 3000;
+const User = require('./db/models/user')(sequelize, Sequelize);
+const Transaction = require('./db/models/transaction')(sequelize, Sequelize, User);
 
+const PORT = process.env.SERVER_PORT || 3333;
+
+// for now just create a single test user over and over
+// when the app starts
+// User.sync({ force: true })
+//   .then(() =>
+//     User.create({
+//       firstName: 'Test',
+//       lastName: 'User',
+//       password: '',
+//       email: 'test@user.com'
+//     })
+//   )
+//   .catch(err => {
+//     console.log('Error:', err);
+//   });
+
+// sync/create the Transations table
+Transaction.sync();
+
+// Express Middlewaretesttest
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1', router);
 
@@ -37,12 +62,24 @@ router.post('/presto/login', async (req, res) => {
   }
 });
 
-router.post('/presto/usage/:year', async (req, res) => {
+router.get('/presto/usage/:year', async (req, res) => {
   try {
     const usage = await getUsageReport(req.params.year);
+    const testUser = await User.findOne({ where: { firstName: 'test' } });
 
     console.log(`Getting usage report for ${req.params.year}...`);
     res.send(usage);
+    console.log(`Saving usage to db...`);
+    usage.forEach(async transaction => {
+      await Transaction.create({
+        userId: 1,
+        date: new Date(transaction.date),
+        agency: transaction.agency,
+        location: transaction.location,
+        type: transaction.type,
+        amount: transaction.amount.replace(/[($)]/g, '')
+      });
+    });
   } catch (error) {
     res.send({ error });
   }
