3ef0c63314eb6b1c04cabc33a9d416c49ebc83db
Ian McCausland <imccausl@gmail.com>
2019-01-08 23:54:53 -0500

    Add nprogress bar

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,24 @@
 import 'semantic-ui-css/semantic.min.css';
+import 'nprogress/nprogress.css';
 
 import React from 'react';
 import App, { Container } from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 
 import HeaderBar from '../components/styled/HeaderBar';
+import Meta from '../components/Meta';
 
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {};
@@ -20,10 +34,13 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <Container>
-        <HeaderBar />
-        <Component {...pageProps} />
-      </Container>
+      <>
+        <Meta />
+        <Container>
+          <HeaderBar />
+          <Component {...pageProps} />
+        </Container>
+      </>
     );
   }
 }
