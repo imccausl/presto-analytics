35ea072af4901cb499a4c4d5a438fba1c67e5456
Ian McCausland <imccausl@gmail.com>
2019-01-08 21:33:05 -0500

    Remove login component from root

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,7 @@
-import 'semantic-ui-css/semantic.min.css';
-
 import HeaderBar from '../components/styled/HeaderBar';
-import Login from '../components/Login';
 
 export default () => (
   <>
     <HeaderBar />
-    <Login />
   </>
 );
