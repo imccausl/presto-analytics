8bcdcd29cdfc43aa30c28b230883011e5e6dcd28
Ian McCausland <imccausl@gmail.com>
2019-01-08 21:31:03 -0500

    Create login window that communicates with login api endpoint

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,28 +1,78 @@
-import { Input, Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+import {
+  Input, Icon, Button, Card,
+} from 'semantic-ui-react';
 import Fetch from 'react-fetch-component';
 
-export default () => (
-  <div>
-    <div>
-      <Input label="Email" placeholder="Email address" />
-    </div>
-    <div>
-      <Input label="Password" type="password" placeholder="Password" />
-    </div>
-    <Button content="Login" primary />
-    <Fetch
-      url="http://localhost:3333/api/v1"
-      options={{
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: 'gee@test.com', password: 'dfefd' }),
-      }}
-      as="json"
-    >
-      {({ loading, data, error }) => <div />}
-    </Fetch>
-  </div>
-);
+import API from '../util/api';
+
+export default class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  saveToState = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <Fetch manual url={`${API.root}${API.login}`} options={API.send({ email, password })}>
+        {({
+          fetch, loading, data, error,
+        }) => {
+          console.log(error);
+          return (
+            <Card centered raised style={{ width: '400px' }}>
+              <Card.Content>
+                <Card.Header>Sign in</Card.Header>
+                <Card.Description style={{ marginBottom: '15px' }}>
+                  Enter your email address and password below.
+                </Card.Description>
+                <Input
+                  style={{ marginBottom: '5px' }}
+                  fluid
+                  iconPosition="left"
+                  placeholder="Email"
+                >
+                  <Icon name="at" />
+                  <input disabled={loading} type="text" name="email" onChange={this.saveToState} />
+                </Input>
+                <Input fluid iconPosition="left" placeholder="Password">
+                  <Icon name="key" />
+                  <input
+                    disabled={loading}
+                    type="password"
+                    name="password"
+                    onChange={this.saveToState}
+                  />
+                </Input>
+              </Card.Content>
+              <Card.Content extra style={{ textAlign: 'right' }}>
+                <Button
+                  positive
+                  labelPosition="right"
+                  icon="chevron circle right"
+                  content="Login"
+                  disabled={loading}
+                  loading={loading}
+                  onClick={() => {
+                    fetch();
+                    this.setState({ email: '', password: '' });
+                  }}
+                />
+              </Card.Content>
+            </Card>
+          );
+        }}
+      </Fetch>
+    );
+  }
+}
