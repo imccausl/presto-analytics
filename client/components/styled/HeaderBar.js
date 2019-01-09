1015a6c6c1d8741ef9b3009a6ddc6f96abf13c5a
Ian McCausland <imccausl@gmail.com>
2019-01-08 21:32:09 -0500

    Remove ugly green bar

--- client/components/styled/HeaderBar.js
+++ client/components/styled/HeaderBar.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
 const Bar = styled.div`
-  background: #556812;
   width: 100%;
   height: 40px;
 `;
 
-export default () => <Bar>Making ? UI Work?</Bar>;
+export default () => <Bar />;
