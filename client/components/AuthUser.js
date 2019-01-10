bc252c92774860d6288e5ef057606ee090b71086
Ian McCausland <imccausl@gmail.com>
2019-01-09 20:44:16 -0500

    Create component for checking if user is authenticated

---
+++ client/components/AuthUser.js
@@ -0,0 +1,16 @@
+import Fetch from 'react-fetch-component';
+import PropTypes from 'prop-types';
+
+import API from '../util/api';
+
+const AuthUser = props => (
+  <Fetch url={`${API.root}${API.currentUser.endpoint}`} options={API.send(API.currentUser.method)}>
+    {payload => props.children(payload)}
+  </Fetch>
+);
+
+AuthUser.propTypes = {
+  children: PropTypes.func.isRequired,
+};
+
+export default AuthUser;
