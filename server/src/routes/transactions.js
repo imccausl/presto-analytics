e197887eb6ee2a9770402f91da748ec904701cf9
Ian McCausland <imccausl@gmail.com>
2019-01-11 08:06:28 -0500

    Change activity data route to return presto taps only

--- server/src/routes/transactions.js
+++ server/src/routes/transactions.js
@@ -3,26 +3,32 @@ const express = require('express');
 const routes = (Transaction, sequelize, Sequelize) => {
   const router = express.Router();
 
-  router.get('/:year/:month', async (req, res) => {
-    const searchDateMin = `${req.params.year}-${req.params.month}-01`;
-    const searchDateMax =
-      req.params.month === '12' ? `${parseInt(req.params.year, 10) + 1}-01-01` : `${req.params.year}-${parseInt(req.params.month, 10) + 1}-01`;
+  router.get('/:year/:month', async (req, res, next) => {
+    try {
+      const searchDateMin = `${req.params.year}-${req.params.month}-01`;
+      const searchDateMax =
+        req.params.month === '12' ? `${parseInt(req.params.year, 10) + 1}-01-01` : `${req.params.year}-${parseInt(req.params.month, 10) + 1}-01`;
 
-    const transactions = await Transaction.findAll({
-      where: {
-        userId: req.userId || 1, // temporary || for testing until the client is up and running
-        date: {
-          [Sequelize.Op.gte]: new Date(searchDateMin),
-          [Sequelize.Op.lt]: new Date(searchDateMax)
-        }
-      },
-      order: sequelize.literal('date ASC')
-    });
+      const transactions = await Transaction.findAll({
+        where: {
+          userId: req.userId,
+          type: Sequelize.or('Fare Payment', 'Transit Pass Payment'),
+          date: {
+            [Sequelize.Op.gte]: new Date(searchDateMin),
+            [Sequelize.Op.lt]: new Date(searchDateMax)
+          }
+        },
+        order: sequelize.literal('date ASC')
+      });
 
-    const totalAmount = transactions.reduce((sum, trans) => sum + parseFloat(trans.amount), 0);
-    const totalTrips = transactions.length;
+      const totalAmount = transactions.reduce((sum, trans) => sum + parseFloat(trans.amount), 0);
+      const totalTrips = transactions.length;
 
-    res.json({ status: 'success', data: { transactions, totalTrips, totalAmount } });
+      res.json({ status: 'success', data: { transactions, totalTrips, totalAmount } });
+    } catch (err) {
+      console.error(err.stack);
+      next(err);
+    }
   });
 
   return router;
