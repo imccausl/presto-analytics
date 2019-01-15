9d6513b51da1b560ee0e8bbb2b453b62324db3e0
Ian McCausland <imccausl@gmail.com>
2019-01-14 22:22:08 -0500

    Create pie chart that maybe will be used?

---
+++ client/components/dashboard/TransfersVsPayments.js
@@ -0,0 +1,112 @@
+import { Component } from 'react';
+import {
+  ResponsiveContainer, PieChart, Pie, Sector,
+} from 'recharts';
+
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value,
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? 'start' : 'end';
+
+  return (
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        {payload.name}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">
+        {`${value}`}
+      </text>
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+        {`(Rate ${(percent * 100).toFixed(2)}%)`}
+      </text>
+    </g>
+  );
+};
+
+export default class TransfersVsPayments extends Component {
+  state = { activeIndex: 0 };
+
+  onPieEnter = (data, index) => {
+    this.setState({
+      activeIndex: index,
+    });
+  };
+
+  render() {
+    const { data } = this.props;
+    const { activeIndex } = this.state;
+    const sortedData = {};
+
+    data.forEach((item) => {
+      if (!sortedData[item.type]) {
+        sortedData[item.type] = 1;
+      } else {
+        sortedData[item.type] += 1;
+      }
+    });
+
+    const chartData = Object.keys(sortedData).map(key => ({
+      name: key.replace('Payment', ''),
+      value: sortedData[key],
+    }));
+    console.log(chartData);
+    return (
+      <div style={{ margin: '0 20px', padding: '0 20px' }}>
+        <PieChart width={450} height={200}>
+          <Pie
+            activeIndex={activeIndex}
+            activeShape={renderActiveShape}
+            data={chartData}
+            dataKey="value"
+            cx={200}
+            cy={100}
+            innerRadius={40}
+            outerRadius={60}
+            fill="#8884d8"
+            onMouseEnter={this.onPieEnter}
+          />
+        </PieChart>
+      </div>
+    );
+  }
+}
