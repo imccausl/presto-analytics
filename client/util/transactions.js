ae0e2fdfcff1da4a97c53e17a98f399c5799bb58
Ian McCausland <imccausl@gmail.com>
2019-01-15 22:48:52 -0500

    Pull out data breakdown parsing into a seperate function

---
+++ client/util/transactions.js
@@ -0,0 +1,47 @@
+import moment from 'moment';
+
+function totalDailyTransactionBreakdown(transactions, includeAmountInDomain = false) {
+  const dataset = {};
+  const domain = [0, 0];
+  const currYear = new Date(transactions[0].date).getFullYear();
+  const currMonth = new Date(transactions[0].date).getMonth() + 1;
+  const lastDay = new Date(transactions[0].date).getDate();
+
+  for (let i = 1; i <= lastDay; i += 1) {
+    const dateString = `${i < 10 ? `0${i}` : i}/${
+      currMonth < 10 ? `0${currMonth}` : currMonth
+    }/${currYear}`;
+
+    dataset[dateString] = { amount: 0, trips: 0 };
+  }
+
+  transactions.forEach((item) => {
+    const date = moment(item.date).format('DD/MM/YYYY');
+    const amount = parseFloat(item.amount);
+
+    dataset[date].amount += amount;
+    dataset[date].trips += 1;
+  });
+
+  const breakdown = Object.keys(dataset).map((key) => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currDate = moment(key, 'DD/MM/YYYY');
+
+    if (includeAmountInDomain) {
+      domain[1] = domain[1] < dataset[key].amount ? dataset[key].amount : domain[1];
+    }
+    domain[1] = domain[1] < dataset[key].trips ? dataset[key].trips : domain[1];
+
+    return {
+      date: currDate.format('D'),
+      dayOfWeek: days[currDate.day()],
+      amount: dataset[key].amount,
+      trips: dataset[key].trips,
+    };
+  });
+
+  console.log(breakdown);
+  return { dataset: breakdown, domain };
+}
+
+export { totalDailyTransactionBreakdown };
