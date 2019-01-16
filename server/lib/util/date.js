5ec3a3cad97cf11aa03e42df8f7768ac1a8337ab
Ian McCausland <imccausl@gmail.com>
2019-01-15 19:54:53 -0500

    Add quick getDateName utility

---
+++ server/lib/util/date.js
@@ -0,0 +1,9 @@
+function getMonthName(num) {
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  return months[num];
+}
+
+module.exports = {
+  getMonthName
+};
