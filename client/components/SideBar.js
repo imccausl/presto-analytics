076345fd05ced19f8bbb0cc168d8b66f2ba020f4
Ian McCausland <imccausl@gmail.com>
2019-01-16 23:02:03 -0500

    Make a few of the menu items work

--- client/components/SideBar.js
+++ client/components/SideBar.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Menu, Icon } from 'semantic-ui-react';
 import styled from 'styled-components';
 
@@ -6,11 +7,24 @@ const Nav = styled.nav`
   padding: 20px;
 `;
 
+const handleClick = (e) => {
+  // console.dir(e.target);
+  // if (e.target.name === 'Home') {
+  //   Router.push('/dashboard');
+  // } else {
+  //   Router.push('/monthly');
+  // }
+};
+
 export default props => (
   <Nav>
     <Menu secondary vertical>
-      <Menu.Item name="Home" icon="home" active />
-      <Menu.Item name="Budget" icon="usd" />
+      <Link href="/dashboard">
+        <Menu.Item name="Home" icon="home" onClick={handleClick} />
+      </Link>
+      <Link href="/monthly">
+        <Menu.Item name="Monthly Usage" icon="usd" onClick={handleClick} />
+      </Link>
       <Menu.Item name="Reload History" icon="credit card" />
       <Menu.Item name="Trip History" icon="history" />
       <Menu.Item name="All Transactions" icon="list" />
