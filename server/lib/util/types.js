6b4b9ef3c8074b1ebb5a2c3b84ac4301865c5510
Ian McCausland <imccausl@gmail.com>
2019-01-16 22:24:21 -0500

    Move types into a seperate file

---
+++ server/lib/util/types.js
@@ -0,0 +1,8 @@
+const types = {
+  TRANSIT_PASS_LOAD: 'Load Transit Pass',
+  TRANSIT_FARE: 'Fare Payment',
+  TRANSIT_PASS: 'Transit Pass Payment',
+  TRANSFER: 'Transfer'
+};
+
+module.exports = types;
