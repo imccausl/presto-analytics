7c6cc7d2749d4e559dc81941cb9750bd70e242bf
Ian McCausland <imccausl@gmail.com>
2019-01-16 23:02:31 -0500

    Move monthly listing to new route

--- client/components/Page.js
+++ client/components/Page.js
@@ -1,10 +1,10 @@
-import { Dropdown, Icon } from "semantic-ui-react";
-import styled, { ThemeProvider, injectGlobal } from "styled-components";
-import PropTypes from "prop-types";
+import { Dropdown, Icon } from 'semantic-ui-react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import PropTypes from 'prop-types';
 
-import Meta from "./Meta";
-import HeaderBar from "./styled/HeaderBar";
-import SideBar from "./SideBar";
+import Meta from './Meta';
+import HeaderBar from './styled/HeaderBar';
+import SideBar from './SideBar';
 
 const Content = styled.div`
   position: relative;
@@ -16,30 +16,34 @@ const FlexRow = styled.div`
   display: flex;
   flex-direction: row;
   align-items: stretch;
-  justify-content: ${props => props.justify || "stretch"};
-  padding: ${props => props.padding || "0"};
+  justify-content: ${props => props.justify || 'stretch'};
+  padding: ${props => props.padding || '0'};
 `;
 
 const trigger = (
   <span>
-    <Icon name="user" /> Hello, Ian
+    <Icon name="user" />
+    {' '}
+Hello, Ian
   </span>
 );
 
 const options = [
   {
-    key: "user",
+    key: 'user',
     text: (
       <span>
-        Signed in as <strong>Ian McCausland</strong>
+        Signed in as
+        {' '}
+        <strong>Ian McCausland</strong>
       </span>
-    )
+    ),
   },
-  { key: "Details", text: "Account Details" },
-  { key: "Refresh", text: "Refresh Data" }
+  { key: 'Details', text: 'Account Details' },
+  { key: 'Refresh', text: 'Refresh Data' },
 ];
 
-const Page = props => {
+const Page = (props) => {
   const { children } = props;
 
   return (
@@ -49,19 +53,11 @@ const Page = props => {
         <SideBar />
         <Content>
           <HeaderBar>
-            <FlexRow
-              justify="flex-end"
-              padding="5px 20px"
-              style={{ color: "white" }}
-            >
+            <FlexRow justify="flex-end" padding="5px 20px" style={{ color: 'white' }}>
               <Dropdown trigger={trigger} options={options} direction="left" />
             </FlexRow>
           </HeaderBar>
-          <div
-            style={{ position: "relative", zIndex: "10", marginTop: "-460px" }}
-          >
-            {children}
-          </div>
+          <div style={{ position: 'relative', zIndex: '10', marginTop: '-460px' }}>{children}</div>
         </Content>
       </FlexRow>
     </>
