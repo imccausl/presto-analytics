a3ffa4eb3e1eccf943bb42a54e1392f4f41abca3
Ian McCausland <imccausl@gmail.com>
2019-01-26 17:59:48 -0500

    Add error handling

--- client/components/Login.js
+++ client/components/Login.js
@@ -7,6 +7,7 @@ import Fetch from 'react-fetch-component';
 import Link from 'next/link';
 
 import API from '../lib/api';
+import { UserContext } from './Page';
 
 export default class Login extends Component {
   constructor() {
@@ -25,73 +26,84 @@ export default class Login extends Component {
   render() {
     const { email, password } = this.state;
     return (
-      <Fetch manual url={`${API.root}${API.login}`} options={API.send({ email, password })}>
-        {({
-          fetch, data, loading, error,
-        }) => {
-          if (error) {
-            return <div>{error.message}</div>;
-          }
-
-          if (data && data.status === 'success') {
-            return Router.push('/dashboard');
-          }
-
+      <UserContext.Consumer>
+        {(ctx) => {
+          console.log(ctx);
           return (
-            <div style={{ minWidth: '35%' }}>
-              <Message
-                attached
-                header="Log in"
-                content="Enter your email address and password to log in to your account"
-              />
-              <Form className="attached fluid segment">
-                <Form.Input fluid iconPosition="left" placeholder="Email">
-                  <Icon name="at" />
-                  <input
-                    disabled={loading}
-                    type="text"
-                    name="email"
-                    onChange={this.saveToState}
-                    value={email}
-                  />
-                </Form.Input>
-                <Form.Input fluid iconPosition="left" placeholder="Password">
-                  <Icon name="key" />
-                  <input
-                    disabled={loading}
-                    type="password"
-                    name="password"
-                    onChange={this.saveToState}
-                    value={password}
-                  />
-                </Form.Input>
+            <Fetch manual url={`${API.root}${API.login}`} options={API.send({ email, password })}>
+              {({
+                fetch, data, loading, error,
+              }) => {
+                if ((data && data.status === 'success') || (ctx.data && ctx.data.data.user)) {
+                  return Router.push('/dashboard');
+                }
 
-                <div style={{ textAlign: 'right' }}>
-                  <Button
-                    style={{ marginRight: 0 }}
-                    positive
-                    labelPosition="right"
-                    icon="chevron circle right"
-                    content="Login"
-                    disabled={loading || (!email.length || !password.length)}
-                    loading={loading}
-                    onClick={() => {
-                      fetch();
-                      this.setState({ email: '', password: '' });
-                    }}
-                  />
-                </div>
-              </Form>
-              <Message attached="bottom" warning>
-                <Icon name="help" />
-                Don't have an account?
-                <Link href="/register"> Register</Link>
-                &nbsp;instead.
-              </Message>
-            </div>
+                return (
+                  <div>
+                    {error && (
+                      <Message error>
+                        <Message.Header>An error occured</Message.Header>
+                        {error.message}
+                      </Message>
+                    )}
+                    <div style={{ minWidth: '35%' }}>
+                      <Message
+                        attached
+                        header="Log in"
+                        content="Enter your email address and password to log in to your account"
+                      />
+                      <Form className="attached fluid segment">
+                        <Form.Input fluid iconPosition="left" placeholder="Email">
+                          <Icon name="at" />
+                          <input
+                            disabled={loading}
+                            type="text"
+                            name="email"
+                            onChange={this.saveToState}
+                            value={email}
+                          />
+                        </Form.Input>
+                        <Form.Input fluid iconPosition="left" placeholder="Password">
+                          <Icon name="key" />
+                          <input
+                            disabled={loading}
+                            type="password"
+                            name="password"
+                            onChange={this.saveToState}
+                            value={password}
+                          />
+                        </Form.Input>
+
+                        <div style={{ textAlign: 'right' }}>
+                          <Button
+                            style={{ marginRight: 0 }}
+                            positive
+                            labelPosition="right"
+                            icon="chevron circle right"
+                            content="Login"
+                            disabled={loading || (!email.length || !password.length)}
+                            loading={loading}
+                            onClick={() => {
+                              fetch();
+                              this.setState({ email: '', password: '' });
+                            }}
+                          />
+                        </div>
+                      </Form>
+                      <Message attached="bottom" warning>
+                        <Icon name="help" />
+                        Don't have an account?
+                        <Link href="/register"> Register</Link>
+                        &nbsp;instead.
+                      </Message>
+                    </div>
+                  </div>
+                );
+              }}
+            </Fetch>
           );
         }}
-      </Fetch>
+      </UserContext.Consumer>
     );
   }
 }
