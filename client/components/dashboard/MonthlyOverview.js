119fe4057d5f9e1471db092663bb335191c4723d
Ian McCausland <imccausl@gmail.com>
2019-01-26 18:00:36 -0500

    Add better error handling

--- client/components/dashboard/MonthlyOverview.js
+++ client/components/dashboard/MonthlyOverview.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Fetch from 'react-fetch-component';
 import { YearInput, MonthInput } from 'semantic-ui-calendar-react';
 import {
-  Tab, Modal, Button, Segment, Header,
+  Tab, Modal, Button, Segment, Header, Message,
 } from 'semantic-ui-react';
 
 import Statistic from '../styled/Statistic';
@@ -66,66 +66,73 @@ export default class MonthlyOverview extends Component {
           url={`${API.root}${API.monthlyTransactions(year, month + 1)}`}
           options={API.send('GET')}
         >
-          {payload => (
-            <>
-              <Header as="div" attached="top" block>
-                <FlexRow justify="space-between" align="center">
-                  <h3 style={{ color: '#5558c8', marginBottom: '0' }}>Monthly Overview</h3>
-                  <div>
-                    <Tab
-                      menu={{ secondary: true }}
-                      activeIndex={activeIndex}
-                      panes={panes}
-                      onTabChange={(e, tab) => {
-                        if (tab.activeIndex === 0) {
-                          this.setState({
-                            month: thisMonth,
-                            year: thisYear,
-                          });
-                        } else if (tab.activeIndex === 1) {
-                          this.setState({
-                            month: thisMonth === 0 ? 11 : thisMonth - 1,
-                            year: thisMonth === 0 ? thisYear - 1 : thisYear,
-                          });
-                        } else if (tab.activeIndex === 2) {
-                          this.setState({ open: true });
-                        }
+          {(payload) => {
+            console.log(payload.error);
+            if (!payload.loading && payload.error) {
+              return <Message error>{payload.error.message}</Message>;
+            }
 
-                        this.setState({ activeIndex: tab.activeIndex });
-                      }}
-                    />
-                  </div>
-                </FlexRow>
-              </Header>
+            return (
+              <>
+                <Header as="div" attached="top" block>
+                  <FlexRow justify="space-between" align="center">
+                    <h3 style={{ color: '#5558c8', marginBottom: '0' }}>Monthly Overview</h3>
+                    <div>
+                      <Tab
+                        menu={{ secondary: true }}
+                        activeIndex={activeIndex}
+                        panes={panes}
+                        onTabChange={(e, tab) => {
+                          if (tab.activeIndex === 0) {
+                            this.setState({
+                              month: thisMonth,
+                              year: thisYear,
+                            });
+                          } else if (tab.activeIndex === 1) {
+                            this.setState({
+                              month: thisMonth === 0 ? 11 : thisMonth - 1,
+                              year: thisMonth === 0 ? thisYear - 1 : thisYear,
+                            });
+                          } else if (tab.activeIndex === 2) {
+                            this.setState({ open: true });
+                          }
 
-              <Segment style={{ minHeight: '250px' }} attached loading={payload.loading}>
-                {!payload.loading && (
-                  <MonthlyStats
-                    month={getMonthNameFromNum(month)}
-                    year={year}
-                    data={payload.data.data}
-                  />
-                )}
-              </Segment>
-              <Header as="div" attached="bottom" style={{ minHeight: '80px' }}>
-                <FlexRow justify="space-around">
+                          this.setState({ activeIndex: tab.activeIndex });
+                        }}
+                      />
+                    </div>
+                  </FlexRow>
+                </Header>
+
+                <Segment style={{ minHeight: '250px' }} attached loading={payload.loading}>
                   {!payload.loading && (
-                    <Statistic
-                      label="Taps"
-                      labelColor="#5558c8"
-                      value={payload.data.data.totalTrips}
+                    <MonthlyStats
+                      month={getMonthNameFromNum(month)}
+                      year={year}
+                      data={payload.data.data}
                     />
                   )}
+                </Segment>
+                <Header as="div" attached="bottom" style={{ minHeight: '80px' }}>
+                  <FlexRow justify="space-around">
+                    {!payload.loading && (
+                      <Statistic
+                        label="Taps"
+                        labelColor="#5558c8"
+                        value={payload.data.data.totalTrips}
+                      />
+                    )}
 
-                  {payload.loading
-                    ? ''
-                    : getFareTypeCount(payload.data.data.transactions).map(item => (
-                      <Statistic label={item.name} labelColor="#5558c8" value={item.value} />
-                    ))}
-                </FlexRow>
-              </Header>
-            </>
-          )}
+                    {payload.loading
+                      ? ''
+                      : getFareTypeCount(payload.data.data.transactions).map(item => (
+                        <Statistic label={item.name} labelColor="#5558c8" value={item.value} />
+                      ))}
+                  </FlexRow>
+                </Header>
+              </>
+            );
+          }}
         </Fetch>
 
         <Modal size="tiny" open={open} onClose={this.close}>
