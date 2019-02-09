f2bb741fcb6708cac4a4800640ec91cc424bfa9b
Ian McCausland <imccausl@gmail.com>
2019-02-09 11:59:09 -0500

    Increment through steps as each stage is successful

--- client/components/Register.js
+++ client/components/Register.js
@@ -11,31 +11,34 @@ class Register extends Component {
     completedSteps: 0,
   };
 
-  render() {
+  incrementSteps = () => {
     const { activeStep, completedSteps } = this.state;
 
+    this.setState({ activeStep: activeStep + 1, completedSteps: completedSteps + 1 });
+  };
+
+  render() {
+    const { activeStep, completedSteps } = this.state;
+    console.log(completedSteps > 0, activeStep);
     return (
       <Container>
         <Segment basic textAlign="center">
           <Step.Group ordered>
-            <Step active={activeStep === 1} completed={completedSteps > 1}>
-              <Icon name="user" />
+            <Step active={activeStep === 1} completed={completedSteps > 0}>
               <Step.Content>
                 <Step.Title>Sign Up</Step.Title>
                 <Step.Description>Sign up for your account</Step.Description>
               </Step.Content>
             </Step>
 
-            <Step active={activeStep === 2} completed={completedSteps > 2}>
-              <Icon name="subway" />
+            <Step active={activeStep === 2} completed={completedSteps > 1}>
               <Step.Content>
                 <Step.Title>Presto</Step.Title>
                 <Step.Description>Sign in to Presto</Step.Description>
               </Step.Content>
             </Step>
 
-            <Step active={activeStep === 3} completed={completedSteps > 3}>
-              <Icon name="bullseye" />
+            <Step active={activeStep === 3} completed={completedSteps > 2}>
               <Step.Content>
                 <Step.Title>Finalize</Step.Title>
               </Step.Content>
@@ -44,7 +47,9 @@ class Register extends Component {
         </Segment>
 
         <Segment basic>
-          <RegisterAccount />
+          {activeStep === 1 && <RegisterAccount incrementSteps={this.incrementSteps} />}
+          {activeStep === 2 && <div />}
+          {activeStep === 3 && <div />}
         </Segment>
       </Container>
     );
