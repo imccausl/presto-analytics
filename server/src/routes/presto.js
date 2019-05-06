c022061e3c91c1976205053873b0693a7ff090c9
Ian McCausland <ian.mccausland@tophat.com>
2019-05-06 08:21:09 -0400

    WIP: refactor last transaction to return last transaction dates for each card on account

--- server/src/routes/presto.js
+++ server/src/routes/presto.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const moment = require('moment');
 
-const { login, getBasicAccountInfo, isLoggedIn, getActivityByDateRange } = require('../../lib/presto');
+const {
+  login,
+  getBasicAccountInfo,
+  isLoggedIn,
+  getActivityByDateRange
+} = require('../../lib/presto');
 
-const routes = (Transaction, User) => {
+const routes = (Transaction, User, sequelize, Sequelize) => {
   const router = express.Router();
 
   router.post('/login', async (req, res) => {
@@ -55,78 +60,99 @@ const routes = (Transaction, User) => {
   });
 
   router.post('/usage', async (req, res, next) => {
-    try {
-      let { from, to, cards } = req.body;
-      let filterDateString = '';
-      let transactions = [];
-      const filteredUsage = [];
+    // try {
+    const { from, to } = req.body;
+    const filterDateString = '';
+    const transactions = [];
 
-      if (!req.userId) {
-        throw new Error('No user logged in!');
-      }
+    const filteredUsage = [];
+    const cards = req.body.cards.split(',') || [];
 
-      for (let i = 0; i < cards.length; i++) {
-        const cardNumber = cards[i];
-
-        console.log('Getting from card number: ', cardNumber);
-        const lastTransactionDate = await Transaction.max('date', {
-          where: {
-            userId: req.userId,
-            cardNumber
-          }
-        });
-
-        if (lastTransactionDate) {
-          from = moment(lastTransactionDate).format('MM/DD/YYYY');
-          filterDateString = moment(lastTransactionDate).format('MM/DD/YYYY hh:mm:ss A');
-        }
-
-        console.log('lastTransactionDate:', !!lastTransactionDate, filterDateString);
-
-        if (!to) {
-          to = moment().format('MM/DD/YYYY');
-        }
-
-        const usage = await getActivityByDateRange(from, to, cardNumber);
-        console.log(usage);
-        if (usage.status === 'error') {
-          throw new Error(usage.message);
-        }
-        console.log('Checking for duplicates...');
-
-        console.log(`Saving usage to db...`);
-
-        // res.json({ status: 'success', usage: filteredUsage });
-        if (lastTransactionDate) {
-          usage.transactions.forEach(async item => {
-            const transactionDate = await Transaction.findOne({
-              where: {
-                date: moment(item.date, 'MM/DD/YYYY hh:mm:ss A'),
-                cardNumber,
-                userId: req.userId
-              },
-              attributes: ['date']
-            });
-
-            if (!transactionDate) {
-              console.log('Not dupe:', item);
-              item.userId = req.userId;
-              transactions = Transaction.create(item);
-            }
-          });
-        } else {
-          const updatedUsage = usage.transactions.map(item => {
-            item.userId = req.userId;
-            return item;
-          });
-          transactions = await Transaction.bulkCreate(updatedUsage);
-        }
-      }
-
-      res.json({ status: 'success', data: transactions });
-    } catch (error) {
-      next(error);
+    if (!req.userId) {
+      throw new Error('No user logged in!');
     }
+
+    console.log('Getting from card number(s): ', cards);
+
+    const eachCardLastTransaction = await Transaction.findAll({
+      attributes: [
+        'cardNumber',
+        [sequelize.fn('MAX', sequelize.col('date')), 'lastTransactionDate']
+      ],
+      where: {
+        userId: req.userId,
+        cardNumber: {
+          [Sequelize.Op.in]: cards
+        }
+      },
+      group: ['cardNumber']
+    });
+
+    // convert the dates using moment
+    // and create an array with the essential pieces necessary for the next query?
+    // Wondering how necessary it is to loop through everything here
+    // instead of just working with the eachCardLastTransaction object directly.
+    const lastTransactionDates = eachCardLastTransaction.map(row => ({
+      cardNumber: row.cardNumber,
+      lastTransactionDate: moment(row.lastTransactionDate).format('MM/DD/YYYY'),
+      filterDateString: moment(row.lastTransactionDate).format('MM/DD/YYYY hh:mm:ss A')
+    }));
+
+    console.log('lastTransactionDates:', lastTransactionDates);
+
+    // if (lastTransactionDate) {
+    //   from = moment(lastTransactionDate).format('MM/DD/YYYY');
+    //   filterDateString = moment(lastTransactionDate).format('MM/DD/YYYY hh:mm:ss A');
+    // }
+
+    // console.log('lastTransactionDate:', !!lastTransactionDate, filterDateString);
+
+    // if (!to) {
+    //   to = moment().format('MM/DD/YYYY');
+    // }
+
+    // // need to fix this to work with arrays
+    const usage = await getActivityByDateRange(from, to, cards);
+    // console.log(usage);
+    // if (usage.status === 'error') {
+    //   throw new Error(usage.message);
+    // }
+    // console.log('Checking for duplicates...');
+
+    // console.log(`Saving usage to db...`);
+
+    // // res.json({ status: 'success', usage: filteredUsage });
+    // if (lastTransactionDate) {
+    //   usage.transactions.forEach(async item => {
+    //     const transactionDate = await Transaction.findOne({
+    //       where: {
+    //         date: moment(item.date, 'MM/DD/YYYY hh:mm:ss A'),
+    //         cardNumber: {
+    //           [Sequelize.Op.in]: cards
+    //         },
+    //         userId: req.userId
+    //       },
+    //       attributes: ['date']
+    //     });
+
+    //     if (!transactionDate) {
+    //       console.log('Not dupe:', item);
+    //       item.userId = req.userId;
+    //       transactions = Transaction.create(item);
+    //     }
+    //   });
+    // } else {
+    //   const updatedUsage = usage.transactions.map(item => {
+    //     item.userId = req.userId;
+    //     return item;
+    //   });
+    //   transactions = await Transaction.bulkCreate(updatedUsage);
+    // }
+
+    // res.json({ status: 'success', data: transactions });
+    // } catch (error) {
+    //   next(error);
+    // }
   });
 
   return router;
