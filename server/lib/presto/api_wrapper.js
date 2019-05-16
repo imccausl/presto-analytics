34e4a2140bb1e025c29a0f265bd5307392268471
Ian McCausland <imccausl@gmail.com>
2019-05-15 23:01:57 -0400

    Refactor api_wrapper to return prototype-ready functions for maximum this-ness

--- server/lib/presto/api_wrapper.js
+++ server/lib/presto/api_wrapper.js
@@ -3,19 +3,34 @@ const {
   getBasicAccountInfo,
   getUsageReport,
   getActivityByMonth,
+  setCookieJar,
   getActivityByDateRange
 } = require('./activity');
 
-module.exports = requestInstance => ({
+module.exports = requestWrapper => ({
   // auth.js functions
-  login: (username, password) => login(requestInstance, username, password),
-  isLoggedIn: () => isLoggedIn(requestInstance),
-  createCookieJar: () => createCookieJar(requestInstance),
+  login(username, password) {
+    return login.call(this, requestWrapper, username, password);
+  },
+  isLoggedIn() {
+    return isLoggedIn.call(this, requestWrapper);
+  },
+  createCookieJar() {
+    return createCookieJar.call(this, requestWrapper);
+  },
+  setCookieJar(cookies) {
+    return setCookieJar.call(this, requestWrapper, cookies);
+  },
 
   // activity.js functions
-  getBasicAccountInfo: () => getBasicAccountInfo(requestInstance),
-  getUsageReport: (year, jar) => getUsageReport(requestInstance, year, jar),
-  // getActivityByMonth: (year, month) => getActivityByMonth(requestInstance, year, month),
-  getActivityByDateRange: (from, to, cardNumber) =>
-    getActivityByDateRange(requestInstance, from, to, cardNumber)
+  getBasicAccountInfo() {
+    return getBasicAccountInfo.call(this, requestWrapper);
+  },
+  getUsageReport(year, jar) {
+    return getUsageReport.call(this, requestWrapper, year, jar);
+  },
+  // getActivityByMonth: (year, month) => getActivityByMonth(requestWrapper, year, month),
+  getActivityByDateRange(from, to, cardNumber) {
+    return getActivityByDateRange.call(this, requestWrapper, from, to, cardNumber);
+  }
 });
