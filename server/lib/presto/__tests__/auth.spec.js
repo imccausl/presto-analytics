1354c2ab968aca4682f7733dfac6b9b01025918e
Ian McCausland <ian.mccausland@tophat.com>
2019-05-17 20:52:57 -0400

    Add auth tests around CSRF scraping

--- server/lib/presto/__tests__/auth.spec.js
+++ server/lib/presto/__tests__/auth.spec.js
@@ -3,47 +3,43 @@ const nock = require('nock');
 const API = require('./data/nockApiEndpoints');
 const Mock = require('./data/testServerResponses');
 
-const Presto = require('..');
-
-beforeEach(() => {
-  /* CSRF Scrape before the login request */
-  nock(API.baseUrl)
-    .defaultReplyHeaders({ 'Content-Type': 'text/html; charset=utf-8' })
-    .get(API.homepage)
-    .reply(200, Mock.homepage);
-
-  /* Fake Login API Server */
-  nock(API.baseUrl)
-    .post(API.loginEndpoint, Mock.loginRequest)
-    .reply(200, (uri, requestBody) => {
-      const TEST_PASSWORD = 'test';
-      const TEST_USERNAME = 'test';
-      const request = JSON.parse(requestBody);
-
-      if (
-        request.custSecurity.Login === TEST_USERNAME &&
-        request.custSecurity.Password === TEST_PASSWORD
-      ) {
-        return Mock.loginSuccessPayload;
-      }
-
-      return Mock.loginFailedPayload;
-    });
-
-  nock(API.baseUrl)
-    .get(API.dashboard)
-    .reply();
-});
-
-afterEach(() => nock.cleanAll());
+const { AuthError } = require('../errors');
+const Presto = require('../../presto');
 
 describe('authenticate with presto', () => {
+  beforeEach(() => {
+    /* CSRF Scrape before the login request */
+    nock(API.baseUrl)
+      .defaultReplyHeaders({ 'Content-Type': 'text/html; charset=utf-8' })
+      .get(API.homepage)
+      .reply(200, Mock.homepage);
+
+    /* Fake Login API Server */
+    nock(API.baseUrl)
+      .post(API.loginEndpoint, Mock.loginRequest)
+      .reply(200, (uri, requestBody) => {
+        const TEST_PASSWORD = 'test';
+        const TEST_USERNAME = 'test';
+        const request = JSON.parse(requestBody);
+
+        if (
+          request.custSecurity.Login === TEST_USERNAME &&
+          request.custSecurity.Password === TEST_PASSWORD
+        ) {
+          return Mock.loginSuccessPayload;
+        }
+
+        return Mock.loginFailedPayload;
+      });
+  });
+
+  afterEach(() => nock.cleanAll());
+
   test('correct credentials should return success message', async () => {
     const presto = new Presto();
     const response = await presto.login('test', 'test');
 
-    expect(response.statusCode).toBe(200);
-    expect(response.payload).toEqual(Mock.loginSuccessPayload);
+    expect(response).toEqual(Mock.loginSuccessPayload);
   });
 
   test('incorrect credentials should return failed message', async () => {
@@ -55,17 +51,103 @@ describe('authenticate with presto', () => {
   });
 
   test('user is logged in', async () => {
-    const presto = new Presto();
-    const response = await presto.login('test', 'test');
+    nock(API.baseUrl)
+      .get(API.dashboard)
+      .reply(200, Mock.dashboard);
 
-    expect(response.statusCode).toBe(200);
-    expect(response.payload).toEqual({ Result: 'success', message: 'User is logged in.' });
+    const presto = new Presto();
+    const response = await presto.checkLogin();
+
+    expect(response).toEqual({ Result: 'success', message: 'User is logged in' });
   });
 
   test('user is not logged in', async () => {
+    nock(API.baseUrl)
+      .get(API.dashboard)
+      .reply(200, Mock.homepage);
+
+    const presto = new Presto();
+    const response = await presto.checkLogin();
+
+    expect(response).toEqual({ Result: 'failed', message: 'User is not logged in' });
+  });
+});
+
+describe('CSRF scrape', () => {
+  test('CSRF token not found throws auth error', async () => {
+    nock(API.baseUrl)
+      .get(API.homepage)
+      .reply(200, 'FAIL TO LOAD CSRF TOKEN');
+
+    /* Fake Login API Server */
+    nock(API.baseUrl)
+      .post(API.loginEndpoint, Mock.loginRequest)
+      .reply(200, (uri, requestBody) => {
+        const TEST_PASSWORD = 'test';
+        const TEST_USERNAME = 'test';
+        const request = JSON.parse(requestBody);
+
+        if (
+          request.custSecurity.Login === TEST_USERNAME &&
+          request.custSecurity.Password === TEST_PASSWORD
+        ) {
+          return Mock.loginSuccessPayload;
+        }
+
+        return Mock.loginFailedPayload;
+      });
+
+    nock(API.baseUrl)
+      .get(API.dashboard)
+      .reply(200, Mock.dashboard);
+
     const presto = new Presto();
     const response = await presto.login('test', 'test');
 
-    expect(response.payload).toEqual({ Result: 'failed', message: 'User is not logged in' });
+    expect(response).toEqual(new AuthError('Could not retrieve CSRF token'));
+  });
+
+  test('CSRF input elemenet exists but no value attr', async () => {
+    nock(API.baseUrl)
+      .get(API.homepage)
+      .reply(200, Mock.badHomepage);
+
+    /* Fake Login API Server */
+    nock(API.baseUrl)
+      .post(API.loginEndpoint, Mock.loginRequest)
+      .reply(200, (uri, requestBody) => {
+        const TEST_PASSWORD = 'test';
+        const TEST_USERNAME = 'test';
+        const request = JSON.parse(requestBody);
+
+        if (
+          request.custSecurity.Login === TEST_USERNAME &&
+          request.custSecurity.Password === TEST_PASSWORD
+        ) {
+          return Mock.loginSuccessPayload;
+        }
+
+        return Mock.loginFailedPayload;
+      });
+
+    nock(API.baseUrl)
+      .get(API.dashboard)
+      .reply(200, Mock.dashboard);
+
+    const presto = new Presto();
+    const response = await presto.login('test', 'test');
+
+    expect(response).toEqual(new AuthError('Cannot find correct CSRF token'));
+  });
+
+  test('getCSRF() finds correct login token', async () => {
+    nock(API.baseUrl)
+      .get(API.homepage)
+      .reply(200, Mock.homepage);
+
+    const presto = new Presto();
+    const response = await presto.login('test', 'test');
+
+    expect(response).toEqual({ Result: 'success' });
   });
 });
