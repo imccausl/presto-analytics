a49b05a0176bfcd16fece5c52261b9b72e114246
Ian McCausland <ian.mccausland@tophat.com>
2019-05-18 22:21:59 -0400

    Write tests for parseCardActivity utility function

--- server/lib/presto/__tests__/activity/parseCardActivity.spec.js
+++ server/lib/presto/__tests__/activity/parseCardActivity.spec.js
@@ -2,154 +2,153 @@ const { promisify } = require('util');
 const fs = require('fs');
 const path = require('path');
 
-const nock = require('nock');
-const req = require('request');
-
-const API = require('../data/nockApiEndpoints');
 const Mock = require('../data/fakeServerResponses');
 
-const options = { baseUrl: API.baseUrl };
-const request = promisify(req.defaults(options));
-const cj = request.jar();
-
-const { ParseError } = require('../../errors');
+const { ParseError, PrestoError } = require('../../errors');
 const { parseCardActivity } = require('../../activity');
 
 describe('parseCardActivity()', () => {
   const readFile = promisify(fs.readFile);
   const filePath = path.join(__dirname, '../data/pages/card-activity.html');
-  const expectedCardActivity = [
-    {
-      agency: 'Toronto Transit Commission',
-      amount: '3.10',
-      balance: '3.85',
-      cardNumber: '3139856309122658',
-      date: '4/30/2019 8:08:43 PM',
-      discount: '0.00',
-      location: 'BAY STATION',
-      serviceClass: 'Regular',
-      type: 'Fare Payment'
-    },
-    {
-      agency: 'Toronto Transit Commission',
-      amount: '3.10',
-      balance: '6.95',
-      cardNumber: '3139856309122658',
-      date: '4/30/2019 7:29:56 AM',
-      discount: '0.00',
-      location: 'PAPE STATION',
-      serviceClass: 'Regular',
-      type: 'Fare Payment'
-    },
-    {
-      agency: 'Toronto Transit Commission',
-      amount: '3.10',
-      balance: '10.05',
-      cardNumber: '3139856309122658',
-      date: '4/29/2019 5:28:57 PM',
-      discount: '0.00',
-      location: 'BAY STATION',
-      serviceClass: 'Regular',
-      type: 'Fare Payment'
-    },
-    {
-      agency: 'Toronto Transit Commission',
-      amount: '3.10',
-      balance: '13.15',
-      cardNumber: '3139856309122658',
-      date: '4/29/2019 7:41:19 AM',
-      discount: '0.00',
-      location: 'PAPE STATION',
-      serviceClass: 'Regular',
-      type: 'Fare Payment'
-    },
-    {
-      agency: 'Toronto Transit Commission',
-      amount: '3.10',
-      balance: '16.25',
-      cardNumber: '3139856309122658',
-      date: '4/27/2019 2:34:19 PM',
-      discount: '0.00',
-      location: 'ST CLAIR WEST STATION',
-      serviceClass: 'Regular',
-      type: 'Fare Payment'
-    },
-    {
-      agency: 'Toronto Transit Commission',
-      amount: '3.10',
-      balance: '19.35',
-      cardNumber: '3139856309122658',
-      date: '4/27/2019 11:42:28 AM',
-      discount: '0.00',
-      location: 'PAPE STATION',
-      serviceClass: 'Regular',
-      type: 'Fare Payment'
-    },
-    {
-      agency: 'Toronto Transit Commission',
-      amount: '3.10',
-      balance: '22.45',
-      cardNumber: '3139856309122658',
-      date: '4/26/2019 7:43:47 PM',
-      discount: '0.00',
-      location: 'BAY STATION',
-      serviceClass: 'Regular',
-      type: 'Fare Payment'
-    },
-    {
-      agency: 'Toronto Transit Commission',
-      amount: '3.10',
-      balance: '25.55',
-      cardNumber: '3139856309122658',
-      date: '4/26/2019 7:31:55 AM',
-      discount: '0.00',
-      location: 'PAPE STATION',
-      serviceClass: 'Regular',
-      type: 'Fare Payment'
-    },
-    {
-      agency: 'Toronto Transit Commission',
-      amount: '3.10',
-      balance: '28.65',
-      cardNumber: '3139856309122658',
-      date: '4/25/2019 5:56:48 PM',
-      discount: '0.00',
-      location: 'BAY STATION',
-      serviceClass: 'Regular',
-      type: 'Fare Payment'
-    },
-    {
-      agency: 'Toronto Transit Commission',
-      amount: '30.00',
-      balance: '0.00',
-      cardNumber: '3139856309122658',
-      date: '4/25/2019 5:56:19 PM',
-      discount: '0.00',
-      location: 'BAY STATION',
-      serviceClass: '',
-      type: 'Payment By Credit'
-    }
-  ];
 
   test('return a JSON formatted object if all card activity on page', async () => {
     const html = await readFile(filePath, 'utf-8');
     const scrapedData = parseCardActivity(html, '3139856309122658');
+    const expectedCardActivity = [
+      {
+        agency: 'Toronto Transit Commission',
+        amount: '3.10',
+        balance: '3.85',
+        cardNumber: '3139856309122658',
+        date: '4/30/2019 8:08:43 PM',
+        discount: '0.00',
+        location: 'BAY STATION',
+        serviceClass: 'Regular',
+        type: 'Fare Payment'
+      },
+      {
+        agency: 'Toronto Transit Commission',
+        amount: '3.10',
+        balance: '6.95',
+        cardNumber: '3139856309122658',
+        date: '4/30/2019 7:29:56 AM',
+        discount: '0.00',
+        location: 'PAPE STATION',
+        serviceClass: 'Regular',
+        type: 'Fare Payment'
+      },
+      {
+        agency: 'Toronto Transit Commission',
+        amount: '3.10',
+        balance: '10.05',
+        cardNumber: '3139856309122658',
+        date: '4/29/2019 5:28:57 PM',
+        discount: '0.00',
+        location: 'BAY STATION',
+        serviceClass: 'Regular',
+        type: 'Fare Payment'
+      },
+      {
+        agency: 'Toronto Transit Commission',
+        amount: '3.10',
+        balance: '13.15',
+        cardNumber: '3139856309122658',
+        date: '4/29/2019 7:41:19 AM',
+        discount: '0.00',
+        location: 'PAPE STATION',
+        serviceClass: 'Regular',
+        type: 'Fare Payment'
+      },
+      {
+        agency: 'Toronto Transit Commission',
+        amount: '3.10',
+        balance: '16.25',
+        cardNumber: '3139856309122658',
+        date: '4/27/2019 2:34:19 PM',
+        discount: '0.00',
+        location: 'ST CLAIR WEST STATION',
+        serviceClass: 'Regular',
+        type: 'Fare Payment'
+      },
+      {
+        agency: 'Toronto Transit Commission',
+        amount: '3.10',
+        balance: '19.35',
+        cardNumber: '3139856309122658',
+        date: '4/27/2019 11:42:28 AM',
+        discount: '0.00',
+        location: 'PAPE STATION',
+        serviceClass: 'Regular',
+        type: 'Fare Payment'
+      },
+      {
+        agency: 'Toronto Transit Commission',
+        amount: '3.10',
+        balance: '22.45',
+        cardNumber: '3139856309122658',
+        date: '4/26/2019 7:43:47 PM',
+        discount: '0.00',
+        location: 'BAY STATION',
+        serviceClass: 'Regular',
+        type: 'Fare Payment'
+      },
+      {
+        agency: 'Toronto Transit Commission',
+        amount: '3.10',
+        balance: '25.55',
+        cardNumber: '3139856309122658',
+        date: '4/26/2019 7:31:55 AM',
+        discount: '0.00',
+        location: 'PAPE STATION',
+        serviceClass: 'Regular',
+        type: 'Fare Payment'
+      },
+      {
+        agency: 'Toronto Transit Commission',
+        amount: '3.10',
+        balance: '28.65',
+        cardNumber: '3139856309122658',
+        date: '4/25/2019 5:56:48 PM',
+        discount: '0.00',
+        location: 'BAY STATION',
+        serviceClass: 'Regular',
+        type: 'Fare Payment'
+      },
+      {
+        agency: 'Toronto Transit Commission',
+        amount: '30.00',
+        balance: '0.00',
+        cardNumber: '3139856309122658',
+        date: '4/25/2019 5:56:19 PM',
+        discount: '0.00',
+        location: 'BAY STATION',
+        serviceClass: '',
+        type: 'Payment By Credit'
+      }
+    ];
 
     expect(scrapedData).toStrictEqual(expectedCardActivity);
   });
 
-  test('should throw ParseError if required selector a.fareMediaID is missing', () => {
+  test('throws PrestoError if error returned from presto', async () => {
+    const filePathOverride = path.join(__dirname, '../data/pages/card-activity-error.html');
+    const html = await readFile(filePathOverride, 'utf-8');
+
     const parseErrorTest = () => {
-      getCardsAndBalances(Mock.homepage);
+      parseCardActivity(html, '3139856309122658');
     };
-    expect(parseErrorTest).toThrow(new ParseError('FAILURE'));
+    expect(parseErrorTest).toThrow(
+      new PrestoError(
+        `Sorry, but we don't have any transactions for your PRESTO card for the selected month.`
+      )
+    );
   });
 
-  test('should throw ParseError if required selector "#cardNumber" and/or ".dashboard__quantity" is missing', () => {
+  test('throws ParseError if no transaction found on page', () => {
     const parseErrorTest = () => {
-      getCardsAndBalances(Mock.dashboard);
+      parseCardActivity(Mock.homepage, '3139856309122658');
     };
 
-    expect(parseErrorTest).toThrow(new ParseError('FAILURE'));
+    expect(parseErrorTest).toThrow(new ParseError('table#tblTHR tbody tr'));
   });
 });
