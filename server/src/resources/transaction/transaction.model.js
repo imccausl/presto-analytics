f41ffeb20711b470123ffbf38c9abbfbe2e40eff
Ian McCausland <ian.mccausland@tophat.com>
2019-05-25 13:10:16 -0400

    Add scopes outside the model definition to keep it a bit cleaner

--- server/src/resources/transaction/transaction.model.js
+++ server/src/resources/transaction/transaction.model.js
@@ -11,108 +11,98 @@ function convertToCents(dollars) {
 }
 
 module.exports = (sequelize, DataTypes) => {
-  const transactionModel = sequelize.define(
-    'transaction',
-    {
-      userId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'users',
-          key: 'id'
-        },
-        field: 'user_id'
+  const transactionModel = sequelize.define('transaction', {
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id'
       },
-      cardNumber: {
-        type: DataTypes.STRING,
-        field: 'card_number'
-      },
-      date: {
-        type: DataTypes.DATE
-      },
-      agency: {
-        type: DataTypes.STRING
-      },
-      location: {
-        type: DataTypes.STRING
-      },
-      type: {
-        type: DataTypes.STRING
-      },
-      serviceClass: {
-        type: DataTypes.STRING,
-        field: 'service_class'
-      },
-      discount: {
-        type: DataTypes.INTEGER,
-        set(discount) {
-          this.setDataValue('discount', convertToCents(discount));
-        }
-      },
-      amount: {
-        type: DataTypes.INTEGER,
-        set(amount) {
-          this.setDataValue('amount', convertToCents(amount));
-        }
-      },
-      balance: {
-        type: DataTypes.INTEGER,
-        set(balance) {
-          this.setDataValue('balance', convertToCents(balance));
-        }
+      field: 'user_id'
+    },
+    cardNumber: {
+      type: DataTypes.STRING,
+      field: 'card_number'
+    },
+    date: {
+      type: DataTypes.DATE
+    },
+    agency: {
+      type: DataTypes.STRING
+    },
+    location: {
+      type: DataTypes.STRING
+    },
+    type: {
+      type: DataTypes.STRING
+    },
+    serviceClass: {
+      type: DataTypes.STRING,
+      field: 'service_class'
+    },
+    discount: {
+      type: DataTypes.INTEGER,
+      set(discount) {
+        this.setDataValue('discount', convertToCents(discount));
       }
     },
-    {
-      scopes: {
-        currentUser(userId) {
-          return {
-            where: {
-              user_id: userId
-            }
-          };
-        },
-
-        transfers(month, year) {
-          return {
-            where: sequelize.and(
-              sequelize.where(sequelize.literal(`EXTRACT(YEAR FROM date)`), year),
-              sequelize.where(sequelize.literal(`EXTRACT(MONTH FROM date)`), month),
-              {
-                type: types.TRANSFER
-              }
-            )
-          };
-        },
-
-        taps(month, year) {
-          return {
-            where: sequelize.and(
-              sequelize.where(sequelize.literal(`EXTRACT(YEAR FROM date)`), year),
-              sequelize.where(sequelize.literal(`EXTRACT(MONTH FROM date)`), month),
-              {
-                type: {
-                  [Sequelize.Op.in]: [types.TRANSIT_FARE, types.TRANSIT_PASS, types.TRANSFER]
-                }
-              }
-            ),
-            attributes: ['date', 'agency', 'location', 'type', 'amount', 'balance'],
-            order: sequelize.literal('date DESC')
-          };
-        },
-
-        fares(month, year) {
-          return {
-            where: sequelize.and(
-              sequelize.where(sequelize.literal(`EXTRACT(YEAR FROM date)`), year),
-              sequelize.where(sequelize.literal(`EXTRACT(MONTH FROM date)`), month),
-              {
-                type: { [Sequelize.Op.in]: [types.TRANSIT_FARE, types.TRANSIT_PASS] }
-              }
-            )
-          };
-        }
+    amount: {
+      type: DataTypes.INTEGER,
+      set(amount) {
+        this.setDataValue('amount', convertToCents(amount));
+      }
+    },
+    balance: {
+      type: DataTypes.INTEGER,
+      set(balance) {
+        this.setDataValue('balance', convertToCents(balance));
       }
     }
-  );
+  });
+
+  /**
+   * SCOPES
+   */
+
+  transactionModel.addScope('currentUser', userId => ({
+    where: {
+      user_id: userId
+    }
+  }));
+
+  transactionModel.addScope('transfers', (month, year) => ({
+    where: sequelize.and(
+      sequelize.where(sequelize.literal(`EXTRACT(YEAR FROM date)`), year),
+      sequelize.where(sequelize.literal(`EXTRACT(MONTH FROM date)`), month),
+      {
+        type: types.TRANSFER
+      }
+    )
+  }));
+
+  transactionModel.addScope('taps', (month, year) => ({
+    where: sequelize.and(
+      sequelize.where(sequelize.literal(`EXTRACT(YEAR FROM date)`), year),
+      sequelize.where(sequelize.literal(`EXTRACT(MONTH FROM date)`), month),
+      {
+        type: {
+          [Sequelize.Op.in]: [types.TRANSIT_FARE, types.TRANSIT_PASS, types.TRANSFER]
+        }
+      }
+    ),
+    attributes: ['date', 'agency', 'location', 'type', 'amount', 'balance'],
+    order: sequelize.literal('date DESC')
+  }));
+
+  transactionModel.addScope('fares', (month, year) => ({
+    where: sequelize.and(
+      sequelize.where(sequelize.literal(`EXTRACT(YEAR FROM date)`), year),
+      sequelize.where(sequelize.literal(`EXTRACT(MONTH FROM date)`), month),
+      {
+        type: { [Sequelize.Op.in]: [types.TRANSIT_FARE, types.TRANSIT_PASS] }
+      }
+    )
+  }));
 
   return transactionModel;
 };
