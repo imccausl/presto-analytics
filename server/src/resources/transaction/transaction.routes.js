e6ae91f6845c3c7be5589b60b20b3dcd876ce00b
Ian McCausland <ian.mccausland@tophat.com>
2019-05-25 18:38:39 -0400

    Protect routes

--- server/src/resources/transaction/transaction.routes.js
+++ server/src/resources/transaction/transaction.routes.js
@@ -7,10 +7,10 @@ const router = express.Router();
 
 router.route('/monthly/:year/:month').get(requireSignin(controllers.monthly));
 
-router.route('/all').get(controllers.all);
+router.route('/all').get(requireSignin(controllers.all));
 
-router.route('/ytd/data').get(controllers.ytdData);
+router.route('/ytd/data').get(requireSignin(controllers.ytdData));
 
-router.route('/ytd').get(controllers.ytd);
+router.route('/ytd').get(requireSignin(controllers.ytd));
 
 module.exports = router;
