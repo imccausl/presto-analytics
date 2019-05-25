8d3c6d598971fba3b760a898c1808c4cee3becc4
Ian McCausland <ian.mccausland@tophat.com>
2019-05-25 18:44:03 -0400

    Brainstorming part deux

--- server/src/resources/transaction/transaction.routes.js
+++ server/src/resources/transaction/transaction.routes.js
@@ -5,6 +5,21 @@ const requireSignin = require('../../utils/protect');
 
 const router = express.Router();
 
+/**
+ * USE CASES FOR Transaction ROUTES:
+ *  - delete all
+ *  - create all (if we go the presto library on the front-end route)
+ *  - update all (?)
+ *
+ *  - delete all belonging to a card
+ *  - get all belonging to a card
+ *  - update all belonging to a card
+ *  - create all belonging to a card
+ *
+ *  - get all transactions by year
+ * - get transactions by card/year
+ */
+
 router.route('/monthly/:year/:month').get(requireSignin(controllers.monthly));
 
 router.route('/all').get(requireSignin(controllers.all));
