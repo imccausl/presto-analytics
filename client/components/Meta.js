591082bbf93a65d376b265edfe90a66a8b149072
Ian McCausland <ian.mccausland@tophat.com>
2019-05-28 15:20:04 -0400

    Add favicon

--- client/components/Meta.js
+++ client/components/Meta.js
@@ -6,6 +6,10 @@ const Meta = () => (
     <meta charSet="utf-8" />
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
     <link rel="stylesheet" type="text/css" href="/static/fontawesome-free-5.8.2-web/css/all.css" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+    <link rel="manifest" href="/static/favicons/site.webmanifest" />
     <title>Presto Analytics</title>
   </Head>
 );
