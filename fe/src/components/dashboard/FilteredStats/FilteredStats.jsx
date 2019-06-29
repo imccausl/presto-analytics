90c9bf567b94f195be6b107d5fbb9d4e120774f5
Ian McCausland <ian.mccausland@tophat.com>
2019-06-29 10:21:11 -0400

    Formatting fixes

--- fe/src/components/dashboard/FilteredStats/FilteredStats.jsx
+++ fe/src/components/dashboard/FilteredStats/FilteredStats.jsx
@@ -1,9 +1,9 @@
-import moment from "moment";
-import React from "react";
-import PropTypes from "prop-types";
-import { Card } from "semantic-ui-react";
+import moment from 'moment';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card } from 'semantic-ui-react';
 
-import Statistic from "../../styled/Statistic";
+import Statistic from '../../styled/Statistic';
 
 const propTypes = {};
 
@@ -34,9 +34,7 @@ export default function FilteredStats(props) {
     prevTotalAmount = data.data.prevInterval.totalAmount;
 
     totalTaps = data.data.count.transfers + data.data.count.fares;
-    prevTotalTaps =
-      data.data.prevInterval.count.transfers +
-      data.data.prevInterval.count.fares;
+    prevTotalTaps = data.data.prevInterval.count.transfers + data.data.prevInterval.count.fares;
 
     totalFares = data.data.count.fares;
     prevTotalFares = data.data.prevInterval.count.fares;
@@ -46,17 +44,15 @@ export default function FilteredStats(props) {
 
     prevLastDate = data.data.prevInterval.transactions.length
       ? moment(
-          data.data.prevInterval.transactions[
-            data.data.prevInterval.transactions.length - 1
-          ].date
-        )
-          .utcOffset(0)
-          .format("DD MMM YY")
+        data.data.prevInterval.transactions[data.data.prevInterval.transactions.length - 1].date,
+      )
+        .utcOffset(0)
+        .format('DD MMM YY')
       : null;
     prevFirstDate = data.data.prevInterval.transactions.length
       ? moment(data.data.prevInterval.transactions[0].date)
-          .utcOffset(0)
-          .format("MM/DD/YYYY")
+        .utcOffset(0)
+        .format('MM/DD/YYYY')
       : null;
 
     console.log(data.data.prevInterval.transactions.length - 1);
@@ -77,28 +73,24 @@ export default function FilteredStats(props) {
           <Statistic
             label="Spent"
             value={`$${(totalAmount / 100).toFixed(2)}`}
-            extra={`$${Math.abs((totalAmount - prevTotalAmount) / 100).toFixed(
-              2
-            )} ${prevLastDate ? "from period ending " + prevLastDate : ""}`}
-            extraIcon={
-              totalAmount - prevTotalAmount > 0
-                ? "ti-arrow-up"
-                : "ti-arrow-down"
-            }
+            extra={`$${Math.abs((totalAmount - prevTotalAmount) / 100).toFixed(2)} ${
+              prevLastDate ? `from period ending ${prevLastDate}` : ''
+            }`}
+            extraIcon={totalAmount - prevTotalAmount > 0 ? 'ti-arrow-up' : 'ti-arrow-down'}
             iconName="ti-credit-card"
             isCustomIcon
             extraFooter={
               hasLastYear
-                ? `$${Math.abs(
-                    (totalAmount - lastYearTotalAmount) / 100
-                  ).toFixed(2)} from last year`
+                ? `$${Math.abs((totalAmount - lastYearTotalAmount) / 100).toFixed(
+                  2,
+                )} from last year`
                 : null
             }
             extraFooterIcon={
               hasLastYear
                 ? totalAmount - lastYearTotalAmount > 0
-                  ? "ti-arrow-up"
-                  : "ti-arrow-down"
+                  ? 'ti-arrow-up'
+                  : 'ti-arrow-down'
                 : null
             }
           />
@@ -107,23 +99,19 @@ export default function FilteredStats(props) {
             label="Taps"
             value={totalTaps}
             extra={`${Math.abs(totalTaps - prevTotalTaps)} ${
-              prevLastDate ? "from period ending " + prevLastDate : ""
+              prevLastDate ? `from period ending ${prevLastDate}` : ''
             }`}
-            extraIcon={
-              totalTaps - prevTotalTaps > 0 ? "ti-arrow-up" : "ti-arrow-down"
-            }
+            extraIcon={totalTaps - prevTotalTaps > 0 ? 'ti-arrow-up' : 'ti-arrow-down'}
             iconName="ti-hand-drag"
             isCustomIcon
             extraFooter={
-              hasLastYear
-                ? `${Math.abs(totalTaps - lastYearTotalTaps)} from last year`
-                : null
+              hasLastYear ? `${Math.abs(totalTaps - lastYearTotalTaps)} from last year` : null
             }
             extraFooterIcon={
               hasLastYear
                 ? totalTaps - lastYearTotalTaps > 0
-                  ? "ti-arrow-up"
-                  : "ti-arrow-down"
+                  ? 'ti-arrow-up'
+                  : 'ti-arrow-down'
                 : null
             }
           />
@@ -132,23 +120,19 @@ export default function FilteredStats(props) {
             label="Fares"
             value={totalFares}
             extra={`${Math.abs(totalFares - prevTotalFares)} ${
-              prevLastDate ? "from period ending " + prevLastDate : ""
+              prevLastDate ? `from period ending ${prevLastDate}` : ''
             }`}
-            extraIcon={
-              totalFares - prevTotalFares > 0 ? "ti-arrow-up" : "ti-arrow-down"
-            }
+            extraIcon={totalFares - prevTotalFares > 0 ? 'ti-arrow-up' : 'ti-arrow-down'}
             iconName="ti-ticket"
             isCustomIcon
             extraFooter={
-              hasLastYear
-                ? `${Math.abs(totalFares - lastYearTotalFares)} from last year`
-                : null
+              hasLastYear ? `${Math.abs(totalFares - lastYearTotalFares)} from last year` : null
             }
             extraFooterIcon={
               hasLastYear
                 ? totalFares - lastYearTotalFares > 0
-                  ? "ti-arrow-up"
-                  : "ti-arrow-down"
+                  ? 'ti-arrow-up'
+                  : 'ti-arrow-down'
                 : null
             }
           />
@@ -157,27 +141,21 @@ export default function FilteredStats(props) {
             label="Transfers"
             value={totalTransfers}
             extra={`${Math.abs(totalTransfers - prevTotalTransfers)} ${
-              prevLastDate ? "from period ending " + prevLastDate : ""
+              prevLastDate ? `from period ending ${prevLastDate}` : ''
             }`}
-            extraIcon={
-              totalTransfers - prevTotalTransfers > 0
-                ? "ti-arrow-up"
-                : "ti-arrow-down"
-            }
+            extraIcon={totalTransfers - prevTotalTransfers > 0 ? 'ti-arrow-up' : 'ti-arrow-down'}
             iconName="ti-vector"
             isCustomIcon
             extraFooter={
               hasLastYear
-                ? `${Math.abs(
-                    totalTransfers - lastYearTotalTransfers
-                  )} from last year`
+                ? `${Math.abs(totalTransfers - lastYearTotalTransfers)} from last year`
                 : null
             }
             extraFooterIcon={
               hasLastYear
                 ? totalTransfers - lastYearTotalTransfers > 0
-                  ? "ti-arrow-up"
-                  : "ti-arrow-down"
+                  ? 'ti-arrow-up'
+                  : 'ti-arrow-down'
                 : null
             }
           />
