8bd1a235892db72c4039b41ea69f287cd3b460df
Ian McCausland <imccausl@gmail.com>
2019-09-02 20:38:47 -0400

    Add refresh, settings, and logout buttons to sidebar

--- fe/src/components/SideBar.js
+++ fe/src/components/SideBar.js
@@ -1,7 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Menu, Icon, Rail, Segment } from "semantic-ui-react";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Menu, Icon, Rail, Segment,
+} from 'semantic-ui-react';
+import styled from 'styled-components';
+
+import AccountSettings from './AccountSettings';
+import UpdatePresto from './dashboard/UpdatePresto';
+
+import requestApi from '../lib/requestApi';
 
 const Nav = styled.nav`
   height: 100%;
@@ -18,57 +25,92 @@ const handleClick = e => {
 };
 
 // background: 'rgb(38,39,43)', color: 'rgb(209,211,212)'
-export default props => {
-  const { top } = props;
+export default class SideBar extends Component {
+  state = {
+    activeSelection: 'this month',
+    accountModalOpen: false,
+    prestoModalOpen: false,
+  };
 
-  // fontVariant: 'all-small-caps',
-  return (
-    <>
-      <Menu
-        fixed="left"
-        icon="labeled"
-        size="small"
-        compact
-        pointing
-        secondary
-        inverted
-        vertical
-        style={{
-          background: "rgb(46,113,23)",
-          color: "#333845",
-          fontSize: "1rem",
+  handleItemClick = async (e, { name }) => {
+    if (name === 'update') {
+      this.setState({ prestoModalOpen: true });
+    }
 
-          display: "flex",
-          flexDirection: "column",
-          alignContent: "center"
-        }}>
-        <div style={{ margin: "auto 0" }}>
-          <Link href="/dashboard">
-            <Menu.Item active onClick={handleClick}>
-              <Icon name="dashboard" />
-              {/* Dashboard */}
+    if (name === 'logout') {
+      await requestApi.logout();
+      this.props.history.push('/login');
+    }
+
+    if (name === 'settings') {
+      this.setState({ accountModalOpen: true });
+    }
+  };
+
+  handleAccountModalClose = () => {
+    this.setState({ accountModalOpen: false });
+  };
+
+  handlePrestoModalClose = () => {
+    this.setState({ prestoModalOpen: false });
+  };
+
+  render() {
+    const { top, budget } = this.props;
+    const { prestoModalOpen, accountModalOpen } = this.state;
+
+    // fontVariant: 'all-small-caps',
+    return (
+      <>
+        <Menu
+          fixed="left"
+          icon="labeled"
+          size="small"
+          compact
+          pointing
+          secondary
+          inverted
+          vertical
+          style={{
+            background: 'rgb(36,41,46)',
+            color: '#333845',
+            fontSize: '1rem',
+
+            display: 'flex',
+            flexDirection: 'column',
+            alignContent: 'center',
+          }}
+        >
+          <div style={{ margin: 'auto 0', width: '65px' }}>
+            <Menu.Item header>
+              {/* <Icon name="user circle outline" size="large" />{" "}
+            {`  ${firstName} ${lastName}`} */}
             </Menu.Item>
-          </Link>
-          <Link href="/monthly">
-            <Menu.Item onClick={handleClick}>
-              <Icon name="usd" />
-              {/* Taps */}
-            </Menu.Item>
-          </Link>
-          <Menu.Item>
-            <Icon name="credit card" />
-            {/* Transactions */}
-          </Menu.Item>
-          <Menu.Item>
-            <Icon name="history" />
-            {/* Budget */}
-          </Menu.Item>
-          <Menu.Item icon="list">
-            <Icon name="list" />
-            {/* Everything */}
-          </Menu.Item>
-        </div>
-      </Menu>
-    </>
-  );
-};
+
+            <Menu.Menu position="right">
+              <Menu.Item name="update" onClick={this.handleItemClick}>
+                <Icon name="refresh" size="large" />
+              </Menu.Item>
+
+              <Menu.Item name="settings" onClick={this.handleItemClick}>
+                <Icon name="setting" size="large" />
+              </Menu.Item>
+
+              <Menu.Item name="logout" onClick={this.handleItemClick}>
+                <Icon name="log out" size="large" />
+              </Menu.Item>
+            </Menu.Menu>
+          </div>
+        </Menu>
+
+        <UpdatePresto open={prestoModalOpen} close={this.handlePrestoModalClose} />
+        <AccountSettings
+          open={accountModalOpen}
+          user={this.props}
+          budget={budget || {}}
+          close={this.handleAccountModalClose}
+        />
+      </>
+    );
+  }
+}
