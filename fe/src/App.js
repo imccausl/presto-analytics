8ba2f3c40abc2290577792929d047e70c972ddd1
Ian McCausland <imccausl@gmail.com>
2019-09-02 21:37:44 -0400

    Add settings route

--- fe/src/App.js
+++ fe/src/App.js
@@ -6,16 +6,16 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Dashboard from './routes/Dashboard';
 import Login from './routes/Login';
 import Register from './routes/Register';
+import Settings from './routes/Settings';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Route exact path="/" component={Login} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </div>
+      <Route exact path="/" component={Login} />
+      <Route path="/settings" component={Settings} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </Router>
   );
 }
