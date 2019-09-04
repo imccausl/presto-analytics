3d7dcb368bb55e5e17544dd153dd5bdca7f9611d
Ian McCausland <imccausl@gmail.com>
2019-09-04 11:18:37 -0400

    Convert to stateless component

--- fe/src/components/Dashboard.js
+++ fe/src/components/Dashboard.js
@@ -11,99 +11,97 @@ import TapList from './dashboard/TapList';
 import TopTapOrigins from './dashboard/TopTapOrigins';
 import YearOverview from './dashboard/YearOverview';
 
-class Dashboard extends React.Component {
-  render() {
-    const {
-      props: {
-        data: {
-          user,
-          balance,
-          budget,
-          lastActivity,
-          spent: { amount, since },
-        },
+const Dashboard = props => {
+  const {
+    props: {
+      data: {
+        user,
+        balance,
+        budget,
+        lastActivity,
+        spent: { amount, since },
       },
-    } = this.props;
+    },
+  } = props;
 
-    return (
-      <>
-        <Route path="/dashboard/:cardNumber/:searchType/:yearOrRange/:monthOrUnit">
-          {({ match, history }) => {
-            console.log(match, history);
-            let cardNumber;
-            let searchType;
-            let yearOrRange;
-            let monthOrUnit;
+  return (
+    <>
+      <Route path="/dashboard/:cardNumber/:searchType/:yearOrRange/:monthOrUnit">
+        {({ match, history }) => {
+          console.log(match, history);
+          let cardNumber;
+          let searchType;
+          let yearOrRange;
+          let monthOrUnit;
 
-            if (match) {
-              cardNumber = match.params.cardNumber;
-              searchType = match.params.searchType === SEARCH_TYPE_RANGE
-                || match.params.searchType === SEARCH_TYPE_MONTH
-                ? match.params.searchType
-                : 'err';
-              yearOrRange = Math.abs(parseInt(match.params.yearOrRange, 10));
-              monthOrUnit = match.params.monthOrUnit !== 'days'
-                ? Math.abs(parseInt(match.params.monthOrUnit, 10))
-                : 'days';
-            } else {
-              // defaults
-              cardNumber = 'all';
-              searchType = SEARCH_TYPE_RANGE;
-              yearOrRange = 30;
-              monthOrUnit = 'days';
-
-              return (
-                <Redirect
-                  to={`/dashboard/${cardNumber}/${searchType}/${yearOrRange}/${monthOrUnit}`}
-                />
-              );
-            }
+          if (match) {
+            cardNumber = match.params.cardNumber;
+            searchType = match.params.searchType === SEARCH_TYPE_RANGE
+              || match.params.searchType === SEARCH_TYPE_MONTH
+              ? match.params.searchType
+              : 'err';
+            yearOrRange = Math.abs(parseInt(match.params.yearOrRange, 10));
+            monthOrUnit = match.params.monthOrUnit !== 'days'
+              ? Math.abs(parseInt(match.params.monthOrUnit, 10))
+              : 'days';
+          } else {
+            // defaults
+            cardNumber = 'all';
+            searchType = SEARCH_TYPE_RANGE;
+            yearOrRange = 30;
+            monthOrUnit = 'days';
 
             return (
-              <DataFilter
-                cards={user.cards}
-                history={history}
-                match={match}
-                cardNumber={cardNumber}
-                searchType={searchType}
-                yearOrRange={yearOrRange}
-                monthOrUnit={monthOrUnit}
-              >
-                {({ data, error, loading }) => {
-                  console.log(data);
-                  return (
-                    <Grid columns="equal">
-                      <Grid.Row>
-                        <Grid.Column>
-                          <MonthlyOverview
-                            data={data}
-                            error={error}
-                            loading={loading}
-                            budget={budget || {}}
-                          />
-                        </Grid.Column>
-                      </Grid.Row>
-                      <Grid.Row>
-                        <Grid.Column>
-                          <FilteredStats data={data} error={error} loading={loading} />
-                        </Grid.Column>
-                      </Grid.Row>
-                      <Grid.Row>
-                        <Grid.Column>
-                          <TopTapOrigins data={data} error={error} loading={loading} />
-                        </Grid.Column>
-                        <Grid.Column />
-                      </Grid.Row>
-                    </Grid>
-                  );
-                }}
-              </DataFilter>
+              <Redirect
+                to={`/dashboard/${cardNumber}/${searchType}/${yearOrRange}/${monthOrUnit}`}
+              />
             );
-          }}
-        </Route>
-      </>
-    );
-  }
-}
+          }
+
+          return (
+            <DataFilter
+              cards={user.cards}
+              history={history}
+              match={match}
+              cardNumber={cardNumber}
+              searchType={searchType}
+              yearOrRange={yearOrRange}
+              monthOrUnit={monthOrUnit}
+            >
+              {({ data, error, loading }) => {
+                console.log(data);
+                return (
+                  <Grid columns="equal">
+                    <Grid.Row>
+                      <Grid.Column>
+                        <MonthlyOverview
+                          data={data}
+                          error={error}
+                          loading={loading}
+                          budget={budget || {}}
+                        />
+                      </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                      <Grid.Column>
+                        <FilteredStats data={data} error={error} loading={loading} />
+                      </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                      <Grid.Column>
+                        <TopTapOrigins data={data} error={error} loading={loading} />
+                      </Grid.Column>
+                      <Grid.Column />
+                    </Grid.Row>
+                  </Grid>
+                );
+              }}
+            </DataFilter>
+          );
+        }}
+      </Route>
+    </>
+  );
+};
 
 export default Dashboard;
